4a3a5dfda29b86fbc10e58bacdfc655d7bb04cd3
Jefferson S Rodrigues <jefferson2jsr@gmail.com>
2023-01-20 10:01:19 -0300

    Jogo da forca: exibindo palavra

--- src/app/pages/forca/forca.component.ts
+++ src/app/pages/forca/forca.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-forca',
@@ -7,12 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForcaComponent implements OnInit {
 
+  title = 'Jogo da forca';
+  todas: number = 0;
+
+  forca = [];
+  forca_exibir = "";
+  desenho = desenhos[0];
+
   usuario = {
     acertos: 0,
     total: 0,
-    descr: "",
-    cinco: [],
+    palavra: "",
+    palavrasa: "",
     correto: "",
+    letradig: [],
     uresp: "",
     acertou: -1,
     porc: 0
@@ -20,7 +28,51 @@ export class ForcaComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.proximaPalavra();
+  }
+
+  proximaPalavra() {
+    this.usuario.letradig = [];
+    this.usuario.palavra = palavras[Math.floor(Math.random() * palavras.length)].toUpperCase();
+    this.usuario.palavrasa = this.usuario.palavra.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    this.todas = this.usuario.palavrasa.length;
+    this.forca = '_'.repeat(this.todas).split("");
+    this.forca_exibir = this.forca.join(" ");
+
+  }
+
+  proximaLetra(letra: string) {
+    for (let i = 0; i < this.usuario.palavrasa.length; i++) {
+      if (this.usuario.palavrasa[i] == letra.toUpperCase() && this.forca[i] == '_') {
+        this.todas--;
+        this.forca[i] = this.usuario.palavra[i];
+      }
+    }
+    this.forca_exibir = this.forca.join(" ");
+  }
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) { 
+    this.proximaLetra(event.key);
   }
 
 }
+
+const palavras = [
+  'ENGENHEIRO', 'MÉDICA', 'PISCINA', 'BANANA', 'amarelo', 'Caneca', 'Girafa', 'Escola', 'Agnóstico',
+  'Elefante', 'Escola', 'Estojo', 'Geleia', 'Limonada', 'Passarinho', 'Umbigo', 'Acender',
+  'Asterisco', 'Chuveiro', 'COnviVência', 'COração', 'Esquerdo', 'Independência', 'Oftalmologista',
+  'Otorrinolaringologista', 'Paralelepípedo', 'Quarentena', 'Reportagem', 'Temperança', 'Fleumático',
+  'Verossimilhança', 'Banheiro', 'Cachorro', 'Campeonato', 'Corrupção', 'Galáxia', 'Forca',
+  'História', 'Oração', 'Pneumonia', 'Trilogia', 'Xícara', 'Brincadeira', 'Psicologia', 'Universidade'
+];
+
+const desenhos = [
+`
+_____<br>
+|   |<br>
+|<br>
+|
+`
+];
